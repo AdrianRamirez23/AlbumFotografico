b22404909faca9ec4fe1b388aeb92321e602b681
AdrianRamirez0586 <60004140+AdrianRamirez0586@users.noreply.github.com>
2021-01-19 13:01:13 -0500

    Vista de Crear Evento

--- AlbumFotografico/Form1.Designer.cs
+++ AlbumFotografico/Form1.Designer.cs
@@ -48,9 +48,31 @@ namespace AlbumFotografico
             this.btnBuscar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabGestion = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabCrear = new System.Windows.Forms.TabPage();
+            this.btnSubir = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cmbLugares = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabRead = new System.Windows.Forms.TabPage();
+            this.tabUpdate = new System.Windows.Forms.TabPage();
+            this.tabDelete = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.Directorio.SuspendLayout();
             this.tabDirectorio.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picDir)).BeginInit();
+            this.tabGestion.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabCrear.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Directorio
@@ -110,24 +132,26 @@ namespace AlbumFotografico
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(425, 339);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.Size = new System.Drawing.Size(79, 17);
             this.label6.TabIndex = 12;
             this.label6.Text = "Retroceder";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(207, 339);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.Size = new System.Drawing.Size(60, 17);
             this.label5.TabIndex = 11;
             this.label5.Text = "Avanzar";
             // 
             // txtImagen
             // 
-            this.txtImagen.Location = new System.Drawing.Point(509, 270);
+            this.txtImagen.Location = new System.Drawing.Point(509, 274);
             this.txtImagen.Multiline = true;
             this.txtImagen.Name = "txtImagen";
             this.txtImagen.Size = new System.Drawing.Size(208, 59);
@@ -136,15 +160,16 @@ namespace AlbumFotografico
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(506, 254);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(127, 13);
+            this.label4.Size = new System.Drawing.Size(167, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Descripción de la Imágen";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(175, 183);
+            this.textBox4.Location = new System.Drawing.Point(243, 185);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(176, 106);
@@ -153,22 +178,23 @@ namespace AlbumFotografico
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 186);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 185);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.Size = new System.Drawing.Size(150, 17);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Descripción del Lugar";
+            this.label3.Text = "Descripción del Lugar:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(175, 129);
+            this.textBox3.Location = new System.Drawing.Point(243, 129);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(176, 20);
             this.textBox3.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(175, 78);
+            this.textBox2.Location = new System.Drawing.Point(243, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(176, 20);
             this.textBox2.TabIndex = 5;
@@ -176,20 +202,22 @@ namespace AlbumFotografico
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 132);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.Size = new System.Drawing.Size(120, 17);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Lugar del Evento";
+            this.label2.Text = "Lugar del Evento:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 81);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(122, 17);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Fecha del Evento";
+            this.label1.Text = "Fecha del Evento:";
             // 
             // picDir
             // 
@@ -217,6 +245,8 @@ namespace AlbumFotografico
             // 
             // tabGestion
             // 
+            this.tabGestion.Controls.Add(this.label7);
+            this.tabGestion.Controls.Add(this.tabControl1);
             this.tabGestion.Location = new System.Drawing.Point(4, 22);
             this.tabGestion.Name = "tabGestion";
             this.tabGestion.Padding = new System.Windows.Forms.Padding(3);
@@ -224,6 +254,188 @@ namespace AlbumFotografico
             this.tabGestion.TabIndex = 1;
             this.tabGestion.Text = "Gestion";
             this.tabGestion.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(291, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(303, 39);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Sistema de Control de Eventos";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabCrear);
+            this.tabControl1.Controls.Add(this.tabRead);
+            this.tabControl1.Controls.Add(this.tabUpdate);
+            this.tabControl1.Controls.Add(this.tabDelete);
+            this.tabControl1.Location = new System.Drawing.Point(33, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(728, 341);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabCrear
+            // 
+            this.tabCrear.Controls.Add(this.button1);
+            this.tabCrear.Controls.Add(this.textBox6);
+            this.tabCrear.Controls.Add(this.pictureBox1);
+            this.tabCrear.Controls.Add(this.btnSubir);
+            this.tabCrear.Controls.Add(this.label11);
+            this.tabCrear.Controls.Add(this.cmbLugares);
+            this.tabCrear.Controls.Add(this.textBox5);
+            this.tabCrear.Controls.Add(this.dateTimePicker1);
+            this.tabCrear.Controls.Add(this.label10);
+            this.tabCrear.Controls.Add(this.label9);
+            this.tabCrear.Controls.Add(this.label8);
+            this.tabCrear.Location = new System.Drawing.Point(4, 22);
+            this.tabCrear.Name = "tabCrear";
+            this.tabCrear.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCrear.Size = new System.Drawing.Size(720, 315);
+            this.tabCrear.TabIndex = 0;
+            this.tabCrear.Text = "Crear";
+            this.tabCrear.UseVisualStyleBackColor = true;
+            // 
+            // btnSubir
+            // 
+            this.btnSubir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSubir.Location = new System.Drawing.Point(258, 221);
+            this.btnSubir.Name = "btnSubir";
+            this.btnSubir.Size = new System.Drawing.Size(125, 30);
+            this.btnSubir.TabIndex = 7;
+            this.btnSubir.Text = "Subir Imagen";
+            this.btnSubir.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(30, 226);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(216, 18);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Seleccione la Imagen del Lugar:";
+            // 
+            // cmbLugares
+            // 
+            this.cmbLugares.FormattingEnabled = true;
+            this.cmbLugares.Items.AddRange(new object[] {
+            "Oficina - Bogotá",
+            "Oficina - Medellín",
+            "Oficina - Cali",
+            "Oficina - Barranquilla",
+            "Otro"});
+            this.cmbLugares.Location = new System.Drawing.Point(258, 84);
+            this.cmbLugares.Name = "cmbLugares";
+            this.cmbLugares.Size = new System.Drawing.Size(200, 21);
+            this.cmbLugares.TabIndex = 5;
+            this.cmbLugares.Text = "Seleccione Lugar";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(258, 132);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(200, 67);
+            this.textBox5.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(258, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(30, 132);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(222, 18);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Ingrese la Descripción del Lugar:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(30, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(214, 18);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Seleccione el Lugar del Evento:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(30, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 18);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Seleccione la Fecha del Evento:";
+            // 
+            // tabRead
+            // 
+            this.tabRead.Location = new System.Drawing.Point(4, 22);
+            this.tabRead.Name = "tabRead";
+            this.tabRead.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRead.Size = new System.Drawing.Size(720, 315);
+            this.tabRead.TabIndex = 1;
+            this.tabRead.Text = "Leer";
+            this.tabRead.UseVisualStyleBackColor = true;
+            // 
+            // tabUpdate
+            // 
+            this.tabUpdate.Location = new System.Drawing.Point(4, 22);
+            this.tabUpdate.Name = "tabUpdate";
+            this.tabUpdate.Size = new System.Drawing.Size(720, 315);
+            this.tabUpdate.TabIndex = 2;
+            this.tabUpdate.Text = "Actualizar";
+            this.tabUpdate.UseVisualStyleBackColor = true;
+            // 
+            // tabDelete
+            // 
+            this.tabDelete.Location = new System.Drawing.Point(4, 22);
+            this.tabDelete.Name = "tabDelete";
+            this.tabDelete.Size = new System.Drawing.Size(720, 315);
+            this.tabDelete.TabIndex = 3;
+            this.tabDelete.Text = "Eliminar";
+            this.tabDelete.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(494, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 115);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(494, 178);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(194, 80);
+            this.textBox6.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(347, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 42);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Guardar Evento";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -237,6 +449,11 @@ namespace AlbumFotografico
             this.tabDirectorio.ResumeLayout(false);
             this.tabDirectorio.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picDir)).EndInit();
+            this.tabGestion.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabCrear.ResumeLayout(false);
+            this.tabCrear.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -261,6 +478,24 @@ namespace AlbumFotografico
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabCrear;
+        private System.Windows.Forms.TabPage tabRead;
+        private System.Windows.Forms.TabPage tabUpdate;
+        private System.Windows.Forms.TabPage tabDelete;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSubir;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cmbLugares;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
